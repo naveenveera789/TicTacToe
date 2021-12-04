626527b436f3be563ee660c44106beb5c3b41989
naveenveera789 <naveenveera789@gmail.com>
2021-12-04 09:33:06 +0530

    [naveenveera789] add : The creation of Board program is done

---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public void Board()
+        {
+            char[] board = new char[10];
+            for(int i = 0; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
